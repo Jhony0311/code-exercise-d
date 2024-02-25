b0d61d3d47d114dc854210fd066c4824c3a14d63
Jonathan Ortega <jortega0389@gmail.com>
2024-02-25 11:07:06 -0500

    feat: add msw for mocking

---
+++ src/mocks/browser.ts
@@ -0,0 +1,46 @@
+import { HttpResponse, http } from "msw";
+import { setupWorker } from "msw/browser";
+import { users } from "./users";
+
+const handlers = [
+  http.get("/search", ({ request }) => {
+    const url = new URL(request.url);
+    const query = url.searchParams.get("name");
+
+    if (query === null) {
+      return HttpResponse.json(
+        {
+          status: "error",
+          message: "Invalid query params",
+        },
+        { status: 500 }
+      );
+    }
+
+    if (query === "") {
+      return HttpResponse.json(
+        {
+          status: "ok",
+          results: [],
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const search = users.filter((user) => user.name.includes(query));
+
+    return HttpResponse.json(
+      {
+        status: "ok",
+        results: search,
+      },
+      {
+        status: search.length ? 200 : 404,
+      }
+    );
+  }),
+];
+
+export const worker = setupWorker(...handlers);
